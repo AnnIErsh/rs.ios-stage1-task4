b3bfbf8766dbbe5250bb558b267488fe19a476fd
Anna <annaersh1994@gmail.com>
2020-04-13 00:36:52 +0300

    feat: add task2

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.h
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.h
@@ -1,7 +1,17 @@
 #import <Foundation/Foundation.h>
 
 @interface SquareDecomposer : NSObject
+
+@property (nonatomic) NSMutableArray *numbers;
+@property (nonatomic) NSMutableArray *sums;
+
+@property (nonatomic) NSArray *array;
+
+- (BOOL)isNegative:(NSMutableArray*)arr;
+- (void)refreshData;
+- (BOOL)checkSums:(NSMutableArray*)arr withNumber:(NSNumber*)numb;
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number;
+
 @end
 
 
--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -1,8 +1,85 @@
 #import "SquareDecomposer.h"
 
 @implementation SquareDecomposer
+
+
+- (void)refreshData {
+    
+    [self.numbers removeLastObject];
+    [self.sums removeLastObject];
+}
+
+- (void)squaredCheck:(NSNumber*)sum ofNumber:(NSNumber*)num {
+    
+    if (sum.intValue <= 0)
+        return;
+    else
+    {
+
+        num = @((int)sqrt(sum.intValue));
+        
+        if (self.numbers.count > 1)
+        {
+            if ([num isEqualToNumber:[self.numbers lastObject]] || (num.intValue > [[self.numbers lastObject] intValue]))
+            {
+               
+                [self refreshData];
+                num = @([[self.numbers lastObject] intValue] - 1);
+                sum = @([[self.sums lastObject] intValue]);
+                [self refreshData];
+               // NSLog(@"++++");
+            }
+        }
+        
+        
+        [self.numbers addObject:num];
+        [self.sums addObject:sum];
+    
+        
+        sum = @(sum.intValue - (num.intValue * num.intValue));
+        [self squaredCheck:sum ofNumber:num];
+    }
+}
+
+- (BOOL)isNegative:(NSMutableArray*)arr {
+    
+    for (NSNumber *i in arr)
+    {
+        if (i.intValue <= 0)
+            return YES;
+    }
+    return NO;
+}
+
+- (BOOL)checkSums:(NSMutableArray*)arr withNumber:(NSNumber*)numb {
+    
+    NSMutableArray *array = arr;
+    for (int i = 0; i < arr.count; i++)
+    {
+        [array replaceObjectAtIndex:i withObject:@([[array objectAtIndex:i] intValue] * [[array objectAtIndex:i] intValue])];
+    }
+    self.array = array.copy;
+    NSNumber *sum = @([[self.array valueForKeyPath:@"@sum.self"] intValue]);
+    if (sum.intValue == (numb.intValue * numb.intValue))
+        return YES;
+    return NO;
+}
+
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+        
+    self.numbers = @[].mutableCopy;
+    self.sums = @[].mutableCopy;
+    NSNumber *n = @(number.intValue - 1);
+    NSNumber *sum = @(number.intValue * number.intValue - n.intValue * n.intValue);
+    
+    [self squaredCheck:sum ofNumber:n];
+    [self.numbers insertObject:n atIndex:0];
+    [self.numbers sortUsingSelector:@selector(compare:)];
+    NSLog(@"%@", self.numbers);
+    
+    
+    if ([self isNegative:self.numbers] || ![self checkSums:self.numbers.mutableCopy withNumber:number])
+        return nil;
+    return self.numbers.copy;
 }
 @end
