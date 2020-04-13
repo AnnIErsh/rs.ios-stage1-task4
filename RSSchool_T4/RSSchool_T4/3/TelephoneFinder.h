8811a18edd4cacfa3d9184a7c0c0e6dfd1f2bf4e
Anna <annaersh1994@gmail.com>
2020-04-13 04:22:56 +0300

    feat: add task3

--- RSSchool_T4/RSSchool_T4/3/TelephoneFinder.h
+++ RSSchool_T4/RSSchool_T4/3/TelephoneFinder.h
@@ -1,6 +1,15 @@
 #import <Foundation/Foundation.h>
 
 @interface TelephoneFinder : NSObject
+
+@property (nonatomic, strong) NSMutableString *callstring;
+@property (nonatomic, strong) NSString *firstString;
+@property (nonatomic, strong) NSDictionary *keypad;
+@property (nonatomic, strong) NSMutableArray *numbers;
+
+-(void)refreshCallString;
+- (BOOL)ifNotNumber:(NSString*)str;
 - (NSArray <NSString*>*)findAllNumbersFromGivenNumber:(NSString*)number;
+
 @end
 
--- RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
+++ RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
@@ -1,8 +1,74 @@
 #import "TelephoneFinder.h"
 
 @implementation TelephoneFinder
-- (NSArray <NSString*>*)findAllNumbersFromGivenNumber:(NSString*)number {
-    // good luck
-    return @[@"0"];
+
+- (BOOL)ifNotNumber:(NSString*)str {
+    
+    NSCharacterSet* notDigits = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    if ([str rangeOfCharacterFromSet:notDigits].location == NSNotFound)
+        return YES;
+    return NO;
 }
+
+-(void)refreshCallString {
+    
+    self.callstring = self.firstString.mutableCopy;
+}
+
+-(void)complectNumbers:(NSMutableArray*)numbs withString:(NSString*)str {
+    
+    if (numbs.count <= 0)
+        return;
+    NSString *key = [numbs lastObject];
+    NSArray *array = [self.keypad objectForKey:key];
+    
+    for (NSString *s in array)
+    {
+        [self refreshCallString];
+        [self.callstring replaceCharactersInRange:NSMakeRange(numbs.count - 1, 1) withString:s];
+        [self.numbers addObject:self.callstring.copy];
+    }
+    
+    str = [str substringToIndex:str.length - (str.length > 0)];
+    [numbs removeLastObject];
+    [self complectNumbers:numbs withString:str];
+}
+
+- (NSArray <NSString*>*)findAllNumbersFromGivenNumber:(NSString*)number {
+    
+    if ([self ifNotNumber:number] == NO)
+        return nil;
+    
+    self.firstString = number;
+    self.numbers = @[].mutableCopy;
+    self.callstring = number.mutableCopy;
+    NSMutableArray *callMe = [NSMutableArray new];
+    
+    for (int i=0; i < number.length; i++) {
+        NSString *ch  = [NSString stringWithFormat:@"%c", [number characterAtIndex:i]];
+        [callMe addObject:ch];
+    }
+    
+    NSLog(@"%@", callMe);
+    
+    self.keypad = @{@"1":@[@"2", @"4"],
+                    @"2":@[@"1", @"3", @"5"],
+                    @"3":@[@"2", @"6"],
+                    @"4":@[@"1", @"5", @"7"],
+                    @"5":@[@"2", @"4", @"6", @"8"],
+                    @"6":@[@"3", @"5", @"9"],
+                    @"7":@[@"4", @"8"],
+                    @"8":@[@"0", @"5", @"7", @"9"],
+                    @"9":@[@"6", @"8"],
+                    @"0":@[@"8"]};
+    
+    NSArray *values = [self.keypad objectForKey:number];
+    
+    if (number.length == 1)
+        return values;
+    
+    [self complectNumbers:callMe withString:number.mutableCopy];
+    return self.numbers.copy;
+}
+
 @end
